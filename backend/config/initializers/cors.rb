1705531d2012b4999f464f57018a1b727e9d9d38
TamaGo <ahmadrizqip10@gmail.com>
2025-10-28 19:29:03 +0700

    update cors

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -3,7 +3,19 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     # Ganti origin sesuai alamat frontend kamu
-    origins '*'
+    origins [
+      'http://localhost',
+      'http://localhost:80',
+      'http://localhost:3000',
+      'http://localhost:5173',
+      'http://127.0.0.1:5173',
+      'http://tamago.web.id',
+      'https://tamago.web.id', 
+      'http://www.tamago.web.id',
+      'https://www.tamago.web.id',
+      'http://109.110.188.172',
+      'https://109.110.188.172'
+    ]
 
     resource '*',
       headers: :any,
